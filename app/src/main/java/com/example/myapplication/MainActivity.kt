8d2738da40b0b5a653d73698ecaf9015bfb52fea
alest747 <hoigot747@gmail.com>
2022-09-17 14:23:49 +0800

    first commit

---
+++ app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,39 @@
+package com.example.myapplication
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    var tvText: TextView? = null
+
+
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+            var alphaList = mutableListOf<Char>()
+            var simvols = 'A'..'Z'
+
+            for(tipe in simvols){
+                alphaList.add(tipe)
+                when(tipe){
+                    in 'A'..'Z' -> print(" $tipe")
+                }
+            }
+
+
+
+
+        tvText = findViewById(R.id.TvText)
+        tvText?.setText(alphaList.toString())
+
+
+    }
+
+
+}
